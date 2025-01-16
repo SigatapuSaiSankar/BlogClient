64e1d546760bd9b76c0c8fc0f3aa475066bfa24c
SigatapuSaiSankar <111736268+SigatapuSaiSankar@users.noreply.github.com>
2025-01-16 20:20:03 +0530

    Update Config.js

--- src/utills/Config.js
+++ src/utills/Config.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://blogserver-8k20.onrender.com";
 
 export let token = localStorage.getItem('token');
